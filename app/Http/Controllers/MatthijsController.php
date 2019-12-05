629e324e699caf51e61b48f9ef0bc998a493001d
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-12-05 11:07:45 +0100

    created the matthijs blade page

---
+++ app/Http/Controllers/MatthijsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MatthijsController extends Controller
+{
+    public function index() {
+        return 'Nu zitten we in Matthijscontroller';
+    }
+}
+ 
