0e968a853480d106a1e2900864afc5f68b43e7ab
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-12-05 11:07:30 +0100

    changed the web page

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/* */
 
 Route::get('/', function () {
     return view('welcome');
+})->name('welcome');
+
+Route::get('/matthijs', function () {
+    return view('matthijs');
 });
-
-Route::get('/contact', function() {
-    return view('contact');
-}); 
-
